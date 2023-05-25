49018678c40b3158b46269745b6ee26946020a49
GS <GS@DESKTOP-SLCRNMN>
2023-05-26 00:03:47 +0500

    Add project files.

---
+++ time table project/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace time_table_project
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ time table project/TimeTableDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace time_table_project
+{
+    public class TimeTableDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        
+    }
+}
